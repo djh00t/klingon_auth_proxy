a60655d393a13ab8f33811710c26c7c743e9e985
David Hooton <david@hooton.org>
2023-11-12 22:26:31 +0000

    Add login page
    Signed-off-by: David Hooton <david@hooton.org>

--- tests/test_login_app.py
+++ tests/test_login_app.py
@@ -12,7 +12,7 @@ class TestLoginApp(unittest.TestCase):
 
     def test_login_failure(self):
         # Test login with incorrect credentials
-        response = self.client.post('/login', data={'username': 'admin', 'password': 'wrongpassword'})
+        response = self.client.post('/login', data={'username': 'test-admin', 'password': 'wrongpassword'})
         # Check for 401 Unauthorized
         self.assertEqual(response.status_code, 401)
 
