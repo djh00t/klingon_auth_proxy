2bd58c1a9ac0e7f8de1e891e63a65a41c41ce967
David Hooton <david@hooton.org>
2023-11-12 05:22:20 +0000

    Refactored login function to check credentials against contents of .htaccess file and create token if valid.

--- src/login_app.py
+++ src/login_app.py
@@ -33,31 +33,53 @@ def get_or_create_secret_key(file_path):
         return secret_key
 
 SECRET_KEY = get_or_create_secret_key("secret.key")
+HTACCESS_FILE = "./secrets"
 
-# Dummy user data - replace with your user validation logic
-USERS = {"admin": "password123"}
+def check_credentials(username, password):
+    """
+    Check if the given username and password are valid by comparing them to the contents of the .htaccess file.
+    
+    Args:
+        username (str): The username to check.
+        password (str): The password to check.
+    
+    Returns:
+        bool: True if the username and password are valid, False otherwise.
+    """
+    with open(HTACCESS_FILE, 'r') as file:
+        for line in file:
+            if line.strip():
+                valid_user, valid_pass = line.strip().split(':', 1)
+                if username == valid_user and password == valid_pass:
+                    return True
+    return False
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
-    Logs in the user by checking the credentials provided in the request form.
-    If the credentials are valid, it generates a JWT token and sets it as a cookie in the response.
+    This function handles the login process. It accepts both GET and POST requests.
+    If the request is a POST request, it checks the credentials and creates a token if they are valid.
     If the credentials are invalid, it returns a 401 error.
+    If the request is a GET request, it renders the login.html template.
     """
-    
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
-        if username in USERS and USERS[username] == password:
-            token = jwt.encode({'user': username}, SECRET_KEY, algorithm="HS256")
+        if check_credentials(username, password):
+            # Create a token and set it in a cookie
+            token = jwt.encode(
+                {'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)},
+                SECRET_KEY,
+                algorithm="HS256"
+            )
             resp = make_response(redirect('/'))
             resp.set_cookie('auth_token', token)
             return resp
         else:
-            return 'Invalid credentials', 401
+            return "Invalid credentials", 401
 
-    return render_template('login.html')  # Create a login.html template
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
